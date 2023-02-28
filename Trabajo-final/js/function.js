9edaf11d0f350f6dd26db3b01ad85b123bb77e8e
Fernando Mateos <nando.mateos@gmail.com>
2023-02-27 22:51:16 -0300

    casi terminado-entrega final

--- Trabajo-final/js/function.js
+++ Trabajo-final/js/function.js
@@ -1,4 +1,24 @@
 
+//intenté por todas las formas posibles conseguir la info de la API de randomuser.me pero solo llego hasta poder obtener la info en la consola, y en formato string./*
+$(document).ready(function (){
+      $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    const obj = data;
+    const myJSON = JSON.stringify(obj);
+    console.log(myJSON);
+
+    const API = JSON.stringify(obj);
+    const objeto = JSON.parse(API);
+    document.getElementById("nombre").innerHTML = objeto.results[0].name;
+
+  }
+});
+})
+
+
 function myFunction1() {
         var element = document.getElementById("body");
         element.classList.add("mystyle1")
